ad4598fbd286ecb737c713b55dd040cfb4de270a
Andrew Fleet <mr.andrewjfleet@gmail.com>
2021-06-18 16:46:37 -0400

    Update Reciever.ino
    Adding code initializing the PCA9685 and setting servo 0 to position 0

--- Reciever/Reciever.ino
+++ Reciever/Reciever.ino
@@ -1,12 +1,18 @@
 #include "HCPCA9685.h"
 #include "math.h"
 
-void setup() {
-  // put your setup code here, to run once:
+/* I2C slave address for the device/module. Default I2C address is 0x40 */
+#define  I2CAdd 0x40
 
+/* Create an instance of the library */
+HCPCA9685 HCPCA9685(I2CAdd);
+
+void setup() {
+  HCPCA9685.Init(SERVO_MODE); // Initialize library
+  HCPCA9685.Sleep(false); // Wake device up
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
+  HCPCA9685.Servo(0, 0); // Servo 0, Position 0
+  delay(10);
 }
