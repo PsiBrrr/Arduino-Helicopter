b0edcaa3c146b3ac532ed4f0a91f6c2148b28348
Andrew Fleet <mr.andrewjfleet@gmail.com>
2021-06-18 16:51:29 -0400

    Update Reciever.ino
    Initialize nRF24L01 library

--- Reciever/Reciever.ino
+++ Reciever/Reciever.ino
@@ -7,15 +7,16 @@
 /* I2C slave address for the device/module. Default I2C address is 0x40 */
 #define  I2CAdd 0x40
 
-/* Create an instance of the library */
-HCPCA9685 HCPCA9685(I2CAdd);
+
+HCPCA9685 HCPCA9685(I2CAdd); //Create an instance of the PCA9685 library
+RF24 radio(7,8); //Create an instance of nRF24L01 library using pins 7(CE) and 8(CSN)
 
 void setup() {
-  HCPCA9685.Init(SERVO_MODE); // Initialize library
-  HCPCA9685.Sleep(false); // Wake device up
+  HCPCA9685.Init(SERVO_MODE); //Initialize library
+  HCPCA9685.Sleep(false); //Wake device up
 }
 
 void loop() {
-  HCPCA9685.Servo(0, 0); // Servo 0, Position 0
+  HCPCA9685.Servo(0, 0); //Servo 0, Position 0
   delay(10);
 }
